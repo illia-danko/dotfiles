249e56ad790499480df6bd318ed6da4319190d88
Elijah Danko <me@elijahdanko.net>
2022-08-17 17:28:42 +0200

    Qtile: add copyq to floating window list

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -204,12 +204,7 @@ cursor_warp = False
 floating_layout = layout.Floating(float_rules=[
     # Run the utility of `xprop` to see the wm class and name of an X client.
     *layout.Floating.default_float_rules,
-    Match(wm_class="confirmreset"),  # gitk
-    Match(wm_class="makebranch"),  # gitk
-    Match(wm_class="maketag"),  # gitk
-    Match(wm_class="ssh-askpass"),  # ssh-askpass
-    Match(title="branchdialog"),  # gitk
-    Match(title="pinentry"),  # GPG key password entry
+    Match(wm_class="copyq"),  # copyq
 ])
 auto_fullscreen = True
 focus_on_window_activation = "smart"
