a1f56543889ab5df56dceeacd790b4fc58fc08ee
Illia Danko <me@elijahdanko.net>
2022-11-09 12:09:04 +0100

    Fzf-projects: show relative path output

--- home/fzf-projects.zsh
+++ home/fzf-projects.zsh
@@ -68,13 +68,13 @@ function fzf-projects {
         --preview="tree -C -L 1 $FZF_PROJECTS_ROOT_DIR/{}" \
         --preview-window=$(_fzf_projects_preview_window))
 
-    if [ -d "$line" ]; then
+    if [ -d "$FZF_PROJECTS_ROOT_DIR/$line" ]; then
         if [ "$#" -gt 0 ]; then
             case $1 in
                 '--print') printf "%s\n" "$line";;
             esac
         else
-            cd "$line"
+            cd "$FZF_PROJECTS_ROOT_DIR/$line"
         fi
     fi
 
