377f1802ef93219d0842941f6b80bf0de293095f
illiadanko <illia.a.danko@gmail.com>
2022-12-08 19:43:35 +0100

    Cleanup unused aliases

--- home/shell-aliases/common.sh
+++ home/shell-aliases/common.sh
@@ -9,16 +9,9 @@
 [ -x "$(command -v ip)" ] && alias ip='ip -color=auto'
 [ -x "$(command -v ls)" ] && alias ls='ls --color=auto'
 [ -x "$(command -v bc)" ] && alias bc="bc -l"
-[ -x "$(command -v rlwrap)" ] && [ -x "$(command -v sbcl)" ] && alias sbcl="rlwrap sbcl"
-[ -x "$(command -v rlwrap)" ] && [ -x "$(command -v bb)" ] && alias bb-repl="rlwrap bb --nrepl-server"
-[ -x "$(command -v clj)" ] && alias clj-repl="clj -M:cider/nrepl"
-[ -x "$(command -v rlwrap)" ] && [ -x "$(command -v bb)" ] && alias bb="rlwrap bb"
-[ -x "$(command -v todo.sh)" ] && alias t="todo.sh"
 [ -x "$(command -v dmesg)" ] && alias dmesg='dmesg --color=always | less'
-[ -x "$(command -v clj)" ] && alias clj-repl="clj -M:cider/nrepl"
 [ -x "$(command -v fdfind)" ] && alias fd="fdfind"
 [ -x "$(command -v newsboat)" ] && alias nb="newsboat"
-[ -x "$(command -v neomutt)" ] && alias mutt="neomutt"
 [ -x "$(command -v tmux)" ] && alias t="tmux new -d -s HACK; tmux new -d -s WORK; tmux new -d -s MEDIA; tmux attach -t HACK"
 
 # Override man command.
