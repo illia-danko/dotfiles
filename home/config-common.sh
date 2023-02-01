77663121e2682a901b27449f9e91b810e0167c2e
illiadanko <illia.a.danko@gmail.com>
2023-02-01 13:25:07 +0100

    Use markdown docs over org

--- home/config-common.sh
+++ home/config-common.sh
@@ -60,5 +60,5 @@ if [ -x "$(command -v fzf)" ]; then
 
     export FZF_PREVIEW_COMMAND="cat {}"
     export FZF_DEFAULT_COMMAND="rg --files --hidden -g '!{.git,.svn,.hg,CVS,.bzr,vendor,node_modules,dist,venv,elm-stuff,.clj-kondo,.lsp,.cpcache}'"
-    export FZF_NOTES_DIR="$HOME/github.com/illia-danko/org"
+    export FZF_NOTES_DIR="$HOME/github.com/illia-danko/docs"
 fi
