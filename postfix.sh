056fcf52bc3744eb08e6a8d8c829919915988508
illiadanko <illia.a.danko@gmail.com>
2023-04-14 13:46:14 +0200

    Use Emacs editor

--- postfix.sh
+++ postfix.sh
@@ -2,6 +2,11 @@
 
 set -eo pipefail
 
+script_name="$(readlink -f "${BASH_SOURCE[0]}")"
+script_dir="$(dirname "$script_name")"
+
+tic -x -o ~/.terminfo "$script_dir"/assets/emacs/terminfo-custom.src
+
 if [ "$(uname)" != "Darwin" ]; then
     sudo virsh net-autostart default  # libvirt connection
 fi
