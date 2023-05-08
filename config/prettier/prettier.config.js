671f8eeeccec2e6d9fdfadfac0bd9f535c688179
illiadanko <illia.a.danko@gmail.com>
2023-05-08 18:53:01 +0200

    prettier.config.js: indent level 2

--- config/prettier/prettier.config.js
+++ config/prettier/prettier.config.js
@@ -1,37 +1,37 @@
 module.exports = {
-    trailingComma: "es5",
-    tabWidth: 2,
-    printWidth: 120,
-    semi: true,
-    singleQuote: false,
-    useTabs: false,
-    quoteProps: "as-needed",
-    jsxSingleQuote: false,
-    bracketSameLine: true,
-    arrowParens: "avoid",
-    requirePragma: false,
-    insertPragma: false,
-    proseWrap: "preserve",
-    endOfLine: "lf",
-    overrides: [
-        {
-            files: ["*.yml", "*.yaml"],
-            options: {
-                tabWidth: 2,
-                bracketSpacing: false,
-            },
-        },
-        {
-            files: ["*.json"],
-            options: {
-                tabWidth: 2,
-            },
-        },
-        {
-            files: "*.md",
-            options: {
-                tabWidth: 1,
-            },
-        },
-    ],
+  trailingComma: "es5",
+  tabWidth: 2,
+  printWidth: 120,
+  semi: true,
+  singleQuote: false,
+  useTabs: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  bracketSameLine: true,
+  arrowParens: "avoid",
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: "preserve",
+  endOfLine: "lf",
+  overrides: [
+    {
+      files: ["*.yml", "*.yaml"],
+      options: {
+        tabWidth: 2,
+        bracketSpacing: false,
+      },
+    },
+    {
+      files: ["*.json"],
+      options: {
+        tabWidth: 2,
+      },
+    },
+    {
+      files: "*.md",
+      options: {
+        tabWidth: 1,
+      },
+    },
+  ],
 };
