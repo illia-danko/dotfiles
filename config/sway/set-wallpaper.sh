cacd17c649e0ac5d1881a8cf5c14470b9afb8224
illiadanko <illia.a.danko@gmail.com>
2023-10-03 19:43:02 +0300

    Sway: update theme

--- config/sway/set-wallpaper.sh
+++ config/sway/set-wallpaper.sh
@@ -6,7 +6,7 @@ theme="$(cat "$HOME"/.config/appearance/background)"
 
 wallpapers_dir="$HOME/github.com/illia-danko/wallpapers"
 img_light=0080.jpg
-img_dark=0310.jpg
+img_dark=0012.jpg
 current_img="$wallpapers_dir/$img_light"
 if [ "$theme" == dark ]; then current_img="$wallpapers_dir/$img_dark"; fi
 bg_command_params="output * bg $current_img fill"
