007765ac852d3c1a4c9e6c7962ba9443e531ebb8
Illia Danko <illia.danko@dentsplysirona.com>
2023-11-02 14:11:14 +0200

    Fzf-projects: add bitbucket.org to path

--- home/config-common.sh
+++ home/config-common.sh
@@ -120,7 +120,14 @@ if [ -x "$(command -v fzf)" ]; then
     export RG_OPTS_FILTER='--hidden --glob=!{.git,.svn,.hg,CVS,.bzr,vendor,node_modules,dist,venv,elm-stuff,.clj-kondo,.lsp,.cpcache}'
     export FZF_DEFAULT_COMMAND="rg --files $RG_OPTS_FILTER"
     export FZF_NOTES_DIR="$HOME/github.com/illia-danko/org"
-    export FZF_PROJECTS_ROOT_DIRS="$HOME/github.com $HOME/gitlab.com $HOME/codeburg.org $HOME/.config/nvim $HOME/.local/share/nvim $HOME/bitbucket.org"
+    export FZF_PROJECTS_ROOT_DIRS=" \
+        $HOME/github.com \
+        $HOME/gitlab.com \
+        $HOME/codeburg.org \
+        $HOME/.config/nvim \
+        $HOME/.local/share/nvim \
+        $HOME/bitbucket.org \
+        $HOME/bitbucket.dentsplysirona.com"
     export FZF_PROJECTS_PATTERNS=".git"
 fi
 
