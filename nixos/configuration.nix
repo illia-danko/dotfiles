f2c0cbe1bcd5da034156e2bf9a5c22549b11853a
illiadanko <illia.a.danko@gmail.com>
2023-12-27 15:32:51 +0200

    Update nixos packages

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -181,6 +181,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
     anki
     ansible
     automake
@@ -250,6 +251,7 @@
     ripgrep
     rsync
     shellcheck
+    signal-desktop
     slack
     strace
     stylua
