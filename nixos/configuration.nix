9cd4f5718e8d6fcf7b2ab4035e30bcfef581d9f8
illiadanko <illia.a.danko@gmail.com>
2024-03-15 16:50:33 +0100

    Nixos: update fontconfig

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -133,13 +133,6 @@
     enableDefaultPackages  = true;
     fontDir.enable = true;
     enableGhostscriptFonts = true;
-    fontconfig = {
-        defaultFonts = {
-          serif = [ "Ubuntu" ];
-          sansSerif = [ "Ubuntu" ];
-          monospace = [ "Ubuntu" ];
-        };
-    };
     packages = with pkgs; [
       noto-fonts
       noto-fonts-cjk # Chinese, Japanese, Korean
@@ -153,6 +146,85 @@
       (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
       roboto # Android
     ];
+    fontconfig = {
+      localConf = ''
+        <match target="font">
+            <edit name="antialias" mode="assign">
+                <!-- false,true -->
+                <bool>true</bool>
+            </edit>
+            <edit name="hinting" mode="assign">
+                <!-- false,true -->
+                <bool>true</bool>
+            </edit>
+            <edit name="autohint" mode="assign">
+                <!-- false,true -->
+                <bool>true</bool>
+            </edit>
+            <edit mode="assign" name="hintstyle">
+                <!-- hintnone,hintslight,hintmedium,hintfull -->
+                <const>hintslight</const>
+            </edit>
+            <edit name="rgba" mode="assign">
+                <!-- rgb,bgr,v-rgb,v-bgr -->
+                <const>rgb</const>
+            </edit>
+            <edit mode="assign" name="lcdfilter">
+                <!-- lcddefault,lcdlight,lcdlegacy,lcdnone -->
+                <const>lcddefault</const>
+            </edit>
+
+            <!-- https://wiki.archlinux.org/title/Microsoft_fonts !-->
+            <edit name="embeddedbitmap" mode="assign">
+                <bool>false</bool>
+            </edit>
+        </match>
+
+        <!-- Fallback fonts preference order -->
+        <alias>
+            <family>sans-serif</family>
+            <prefer>
+                <family>Ubuntu</family>
+                <family>Noto Color Emoji</family>
+            </prefer>
+        </alias>
+
+        <alias>
+            <family>serif</family>
+            <prefer>
+                <family>Ubuntu</family>
+                <family>Noto Color Emoji</family>
+            </prefer>
+        </alias>
+        <alias>
+            <family>monospace</family>
+            <prefer>
+                <family>Ubuntu Mono</family>
+                <family>Noto Color Emoji</family>
+            </prefer>
+        </alias>
+
+        <!-- https://wiki.archlinux.org/title/Microsoft_fonts !-->
+        <alias binding="same">
+            <family>Helvetica</family>
+            <accept>
+                <family>Arial</family>
+            </accept>
+        </alias>
+        <alias binding="same">
+            <family>Times</family>
+            <accept>
+                <family>Times New Roman</family>
+            </accept>
+        </alias>
+        <alias binding="same">
+            <family>Courier</family>
+            <accept>
+                <family>Courier New</family>
+            </accept>
+        </alias>
+      '';
+    };
   };
 
   # Allow unfree packages
