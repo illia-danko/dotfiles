ed4650fc0159dea63be226ae9867b91e77fcd118
illiadanko <illia.a.danko@gmail.com>
2024-03-30 01:26:59 +0100

    Minor fix

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,8 +3,8 @@ local wezterm = require("wezterm")
 
 local mux = wezterm.mux
 
-wezterm.on("gui-startup", function()
-  local tab, pane, window = mux.spawn_window({})
+wezterm.on("gui-startup", function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
   window:gui_window():maximize()
 end)
 
