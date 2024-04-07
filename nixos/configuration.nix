b8c6de772009d1e111d842b3adc00b1ab76aa1ab
illiadanko <illia.a.danko@gmail.com>
2024-04-07 23:00:21 +0200

    Enable sway on nixos $2

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -58,9 +58,6 @@
     HandleLidSwitchExternalPower=ignore
     '';
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
   # Configure keymap in X11.
   services.xserver = {
     layout = "us,ua";
