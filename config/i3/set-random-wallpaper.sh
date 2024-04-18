6c24d587ad07494b73ad9f4d5691f523b733c5d1
illiadanko <illia.a.danko@gmail.com>
2024-04-19 02:08:26 +0300

    i3: fix random wallpapers set script

--- config/i3/set-random-wallpaper.sh
+++ config/i3/set-random-wallpaper.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
 random_file_from_dir() {
-    find "$1" -type f | shuf -n 1
+    find "$1" -type f -iname "*.jpg" -o -iname "*.png" | shuf -n 1
 }
 
-nitrogen --set-scaled "$(random_file_from_dir "$1")"
+nitrogen --set-scaled "$(random_file_from_dir "$1")" &> /dev/null
