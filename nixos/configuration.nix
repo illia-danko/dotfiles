6a549f3f363e90155c5667ed5684048825fdbf72
illiadanko <illia.a.danko@gmail.com>
2024-04-20 16:57:40 +0300

    Nixos: add home manager

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,12 +2,13 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
   ];
 
   # Bootloader.
