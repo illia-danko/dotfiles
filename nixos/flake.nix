2da65c81617f1d353f7c287527c47270c025cae5
illiadanko <illia.a.danko@gmail.com>
2024-04-20 18:40:34 +0300

    Nixos: restructure home manager file hierarchy

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -14,7 +14,7 @@
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
-        ./configuration.nix
+        ./hosts/default/configuration.nix
         inputs.home-manager.nixosModules.default
       ];
     };
