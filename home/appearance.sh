40412ecd02b26ac532016985209a9de04477183e
illiadanko <illia.a.danko@gmail.com>
2024-06-25 11:15:14 +0300

    Use same font for all systems

--- home/appearance.sh
+++ home/appearance.sh
@@ -10,11 +10,10 @@ fi
 
 export SYSTEM_COLOR_THEME="$(cat "$s")"
 export TTY_FONT_SIZE="10"
-export TTY_FONT_FAMILY="JetBrainsMonoNL Nerd Font Mono"
+export TTY_FONT_FAMILY="JetBrainsMono Nerd Font Mono"
 export ALACRITTY_WINDOW_DECORATION="None"
 if [ "$(uname)" = "Darwin" ]; then
     export TTY_FONT_SIZE="13"
-    export TTY_FONT_FAMILY="JetBrainsMono Nerd Font Mono"
     export ALACRITTY_WINDOW_DECORATION="Buttonless"
 fi
 
